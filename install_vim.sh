224446c1d14b60b9379d4c5bf1bceb8419208b7e
U-INTRA\hisafumi.a.kikkawa <hisafumi.a.kikkawa@P25680.intra.rakuten.co.jp>
2014-06-01 03:25:06 +0900

    modify procedure

--- install_vim.sh
+++ install_vim.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p ~/.vim/bundle
 #install vim
 curl -O http://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar jxvf vim-7.4.tar.bz2
@@ -15,18 +16,18 @@ cd vim74
     --enable-fail-if-missing
 make
 make install
+cd ..
 
+export PATH="$HOME/.vim/bin/:$PATH"
+alias vi="$HOME/.vim/bin/vim"
 echo 'export PATH="$HOME/.vim/bin/:$PATH"' >> ~/.bashrc
 echo 'alias vi="$HOME/.vim/bin/vim"' >> ~/.bashrc
-. ~/.bashrc
 
 #install neobundle
-mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim 
 
 #set my vimrc
-git clone https://github.com/shinjukujohnny/vimrc.git
-cp vimrc/.vimrc ~/
+cp .vimrc ~/
 
 mkdir ~/.vim/dict
 cd ~/.vim/dict
