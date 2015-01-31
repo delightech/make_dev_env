1ec3ff698cac67eddebd498cd26fdb11c5e95478
吉川　久文 <a13z3hk@pcmacm046.ad.aiit.ac.jp>
2015-01-31 09:40:12 +0900

    modify sh

--- make_dev_env.sh
+++ make_dev_env.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+# update and install yum in advance.
 yum update
 yum -y groupinstall 'Development tools'
 yum -y install ncurses ncurses-devel
+yum -y install python-devel
+yum -y install perl-ExtUtils-Embed
+
+
 ./install_vim.sh
 ./install_perlenv.sh
