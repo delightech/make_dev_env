5b850d03cf11d236cbd0d36f235965e9dcca57b9
Hisafumi Kikkawa <a13z3hk@aiit.ac.jp>
2015-05-07 17:44:50 +0900

    add install in advance

--- make_dev_env.sh
+++ make_dev_env.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# update and install yum in advance.
+# update and install yum in advance for CentOS.
 yum update
 yum -y groupinstall 'Development tools'
 yum -y install ncurses ncurses-devel
 yum -y install python-devel
+yum -y install ruby-devel
 yum -y install perl-ExtUtils-Embed
 
 
