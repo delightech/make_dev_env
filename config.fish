567d3f982c5d73c1c3df3499cb8fa86645dd98ea
delightech <hisafumi.kikkawa@gmail.com>
2019-10-09 10:25:37 +0900

    binにPATHを通す

--- config.fish
+++ config.fish
@@ -2,6 +2,6 @@ set -x PS1 '\u \W $ '
 alias ll='ls -l'
 
 set -x GOPATH $HOME/.go
-set -x PATH $GOPATH $PATH
+set -x PATH $GOPATH/bin $PATH
 alias uri_esc='perl /Users/delightech/work/Utils/src/perl/easy_uri_escape.pl'
 alias uri_unesc='perl /Users/delightech/work/Utils/src/perl/easy_uri_unescape.pl'
